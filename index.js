8e07c0354bf82515cd0ec2d5dededbde463a20f9
stumn <pukut0317@gmail.com>
2023-07-11 00:09:35 +0900

    initial commit (get started & username)

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');     //const 変数 = require('モジュール名')でモジュールを作る
+const app = express();                  //express module でexpress applicationを作る
+const http = require('http');           //http server　を使うためにモジュールを作る？
+const server = http.createServer(app);  //app = express で server を作る？
+const { Server } = require("socket.io");//socket.io のモジュールを作って、socket.io を使ったServer を表現？なぜ｛｝？
+const io = new Server(server);          //http server を引数に、socket.ioが使えるサーバを作り、ioと定義する
+
+// ~/ というurlでリクエストが来たら、/index.htmlというファイルを送るレスポンスをする　reqはなぜ定義されている？
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+// io は接続の全体、socketは接続してきた1つのコマについて
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  let chatLogs;
+  socket.on('nickname', (nickname) => {
+    console.log(nickname);
+    chatLogs += nickname;
+  });
+  socket.on('chat message', (msg) => {
+    console.log(msg);
+    chatLogs +=': ';
+    chatLogs += msg;
+    chatLogs = chatLogs.replace('undefined', 'はじめましての');
+    io.emit('chatLogs', chatLogs);
+    chatLogs = '';
+  });
+ 
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+}); 
