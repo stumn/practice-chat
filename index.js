93c1fd0d80abb0308272046f788a61491cf81cd6
stumn <pukut0317@gmail.com>
2023-11-28 18:32:43 +0900

    1128 improve

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ const server = http.createServer(app);  //app = express で server を作る？
 const { Server } = require("socket.io");//socket.io のモジュールを作って、socket.io を使ったServer を表現？なぜ｛｝？
 const io = new Server(server);          //http server を引数に、socket.ioが使えるサーバを作り、ioと定義する
 const mongoose = require('mongoose');
-// const { count } = require('console');
 const MONGODB_URL = process.env.MONGODB_URL;
+const PORT = process.env.PORT || 3000;
+
+// ↓　mongoose 接続~準備　↓
 mongoose.connect(MONGODB_URL, { useNewUrlParser: true })
   .then(() => {
     console.log('MongoDB connected');
@@ -16,7 +18,7 @@ mongoose.connect(MONGODB_URL, { useNewUrlParser: true })
     console.error('MongoDB connection error:', err);
   });
 
-// オプション設定
+// mongoose オプション設定
 const options = {
   timestamps: true, // データの作成時刻・更新時刻を記録する
   toObject: { // データの id を使いやすく後で使いやすいようにつけてもらうための設定
@@ -26,27 +28,39 @@ const options = {
   }
 };
 
-// 保存するデータの形を定義する（データの種類が複数ある場合はそれぞれ１つずつ定義する）
+// mongoose 保存するデータの形を定義する（データの種類が複数ある場合はそれぞれ１つずつ定義する）
 const postSchema = new mongoose.Schema({ name: String, msg: String, count: Number }, options);
-// その形式のデータを保存・読み出しするために必要なモデルを作る
+
+// mongoose その形式のデータを保存・読み出しするために必要なモデルを作る
 const Post = mongoose.model("Post", postSchema);
+// ↑　mongoose 接続~準備　↑
 
-async function createNewPost(name, msg, count) {
-  try {
-    const p = await Post.create({ name: name, msg: msg, count: count });
-    console.log("新しい投稿が作成されました:");
-  } catch (e) {
-    console.error("エラーが発生しました:", e);
-  }
-};
-
-// ~/ というurlでリクエストが来たら、/index.htmlというファイルを送るレスポンスをする
+// ~/ というurlでリクエストに、/index.htmlファイルを送るレスポンスを返す
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
+
+// チャットメッセージの関数
+async function createNewPost(socket, name, msg, count) {
+  try {
+    createNewRecode(name, msg, count);
+    console.log("新しい投稿:" + newPost);
+    io.emit('chatLogs', newPost);
+  } catch (error) {
+    console.error('エラーが発生しました', error);
+  }
+}
+
+// チャットメッセージの記録
+async function createNewRecode(name, msg, count) {
+  const newPostData = { name, msg, count };
+  const newPost = await Post.create(newPostData);
+  return newPost;
+}
+
 // オンラインメンバー配列
-let onlines = [];
-let id_onlines = [];
+let onlineUsers = [];
+let ids_onlineUsers = [];
 
 // io は接続の全体、socketは接続してきた1つのコマについて
 io.on('connection', async (socket) => {
@@ -56,79 +70,84 @@ io.on('connection', async (socket) => {
     if (name === '' || name === null) {
       name = '匿名';
     }
-    onlines.push(name);
-    id_onlines.push({ id: socket.id, name: name });
-    io.emit('onlines', onlines);
+    onlineUsers.push(name);
+    ids_onlineUsers.push({ id: socket.id, name: name });
+    io.emit('onlineUsers', onlineUsers);
 
+    // 過去のログを取得
     try {
       const posts = await Post.find({}).limit(10).sort({ _id: -1 }); // 新しい順に10件の投稿を取得
       posts.reverse();
-      const pastLogs = posts.map(post => ({ name: post.name, msg: post.msg })); // nameとmsgだけを抽出して新しい配列を作成
+      const pastLogs = posts.map(post => ({ 
+        _id: post._id,
+        name: post.name,
+        msg: post.msg,
+        count: post.count
+       }));
       console.log(pastLogs);
       socket.emit('pastLogs', pastLogs);
-
-    } catch (e) {
-      console.error(e);
+    } catch (error) {
+      console.error('エラーが発生しました', error);
     }
 
+    // いらっしゃいメッセージ
     const welcome_msg = name + 'さん、いらっしゃい！'
     io.emit('welcome', welcome_msg);
+    createNewRecode(name, welcome_msg, 0);
 
-    createNewPost(name, welcome_msg);
-
+    // タイピングイベント受信＆送信
     socket.on('typing', () => {
-      // console.log(name + ' is typing');
       io.emit('typing', name);
     });
 
-    socket.on('chat message', async (nickname, msg) => {
-      try {
-        if (/^\s*$/.test(nickname)) { nickname = name; }
-        const p = await Post.create({ name: nickname, msg, count: 0 }); // 最初はいいねが0
-        console.log(p);
-        io.emit('chatLogs', p);
-        // io.emit('chatLogs', { name: nickname, msg: msg, count: 0 });
-        // _idは定義されてないよって怒られる💦
-        // let chatMessage = '[' + nickname + '] ' + msg;
-        // io.emit('chatLogs', chatMessage);
-        // createNewPost(name, msg, 0);
-      }
-      catch (e) {
-        console.error(e);
-      }
+    // チャットメッセージ受信＆送信
+    socket.on('chat message', (nickname, msg) => {
+      if (/^\s*$/.test(nickname)) { name = name; } // nicknameが空白のみの場合はnameをそのまま使う
+      else { name = nickname; } //nicknameが入力されている場合はnicknameをnameに代入
+      createNewPost(socket, name, msg, 0);
     });
 
+    // アンケートメッセージ受信＆送信
+    socket.on('submitSurvey', async data => {
+      console.log(data);
+      const msg = data.question + data.options.join(', ');
+      const p = await Post.create({ name, msg, count: 0 });
+      io.emit('survey_msg', p);
+    });
+
+    // いいね受信＆送信
     socket.on('fav', async id => {
       console.log('fav id: ' + id);
       const update = { $inc: { count: 1 } };// countを1増やす
       const options = { new: true }; // 更新後のデータを取得する
       try {
         const p = await Post.findByIdAndUpdate(id, update, options);
-        console.log(p);
         io.emit('fav', p);
       } catch (e) {
         console.error(e);
       }
     });
+  
   });
 
+  // 切断時のイベントハンドラを登録
   socket.on('disconnect', async () => {
     let targetId = socket.id;
-    let targetName = id_onlines.find(obj => obj.id === targetId)?.name;
+    let targetName = ids_onlineUsers.find(obj => obj.id === targetId)?.name;
     console.log(targetName + ' (' + socket.id + ') disconnected');
 
+    // さようならメッセージ
     const bye_msg = targetName + 'さん、またね！';
     io.emit('disconnection', bye_msg);
+    createNewRecode(targetName, bye_msg, 0);
 
-    createNewPost(targetName, bye_msg);
-
-    let onlinesWithoutTarget = onlines.filter(val => val !== targetName);
-    onlines = onlinesWithoutTarget;
-
-    io.emit('onlines', onlines);
+    // オンラインメンバーから削除
+    let onlinesWithoutTarget = onlineUsers.filter(val => val !== targetName);
+    onlineUsers = onlinesWithoutTarget;
+    io.emit('onlineUsers', onlineUsers);
   });
 });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+server.listen(PORT, () => {
+  console.log(`listening on *:${PORT}`);
 });
